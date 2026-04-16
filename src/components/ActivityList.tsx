cfa18e8e8d6a1162c304a8eccdd7ec652c8c690c
Galaceon <antoniogarcialtr@gmail.com>
2026-04-16 19:42:14 +0200

    import context for activities

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,20 +1,11 @@
-import type { Activity } from "../types"
-import { categories } from "../data/categories"
-import { useMemo } from "react"
-import type { Dispatch } from "react"
 import { PencilSquareIcon, XCircleIcon } from "@heroicons/react/24/outline"
-import type { ActivityActions } from "../reducers/activity-reducer"
+import { useActivity } from "../hooks/useActivity"
 
-type ActivityListProps = {
-    activities: Activity[],
-    dispatch: Dispatch<ActivityActions>
-}
+export default function ActivityList() {
 
-export default function ActivityList({activities, dispatch} : ActivityListProps) {
+    const { state, dispatch, isEmptyActivities, categoryName } = useActivity()
+    const { activities } = state
 
-    const categoryName = useMemo(() => (category: Activity['category']) => categories.map(cat => cat.id === category ? cat.name : ''), [activities])
-    
-    const isEmptyActivities = useMemo(() => activities.length === 0, [activities])
     
   return (
     <>
--- src/context/ActivityContext.tsx
+++ src/context/ActivityContext.tsx
@@ -1,5 +1,7 @@
 import { createContext, useReducer, type ReactNode, type ActionDispatch, useMemo } from "react";
 import { activityReducer, initialState, type ActivityActions, type ActivityState } from "../reducers/activity-reducer";
+import { categories } from "../data/categories";
+import type { Activity } from "../types";
 
 type ActivityProviderProps = {
     children: ReactNode
@@ -11,6 +13,8 @@ type ActivityContextProps = {
     caloriesConsumed: number
     caloriesBurned: number
     totalCalories: number
+    categoryName: (category: number) => string[]
+    isEmptyActivities: boolean
 }
 
 export const ActivityContext = createContext<ActivityContextProps>(null!)
@@ -24,13 +28,18 @@ export const ActivityProvider = ({children} : ActivityProviderProps) => {
     const caloriesBurned = useMemo(() => state.activities.reduce((total, activity) => activity.category === 2 ? total + activity.calories : total, 0), [state.activities])
     const totalCalories = useMemo(() => caloriesConsumed - caloriesBurned, [state.activities])
 
+    const categoryName = useMemo(() => (category: Activity['category']) => categories.map(cat => cat.id === category ? cat.name : ''), [state.activities])
+    const isEmptyActivities = useMemo(() => state.activities.length === 0, [state.activities])
+
     return (
         <ActivityContext.Provider value={{
             state,
             dispatch,
             caloriesConsumed,
             caloriesBurned,
-            totalCalories
+            totalCalories,
+            categoryName,
+            isEmptyActivities
         }}>
 
             {children}
