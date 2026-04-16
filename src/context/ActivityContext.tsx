8760b30a572c2bb766528b2e0704660b4a672982
Galaceon <antoniogarcialtr@gmail.com>
2026-04-16 19:13:39 +0200

    creating context for global state

---
+++ src/context/ActivityContext.tsx
@@ -0,0 +1,28 @@
+import { createContext, useReducer, type ReactNode, type ActionDispatch } from "react";
+import { activityReducer, initialState, type ActivityActions, type ActivityState } from "../reducers/activity-reducer";
+
+type ActivityProviderProps = {
+    children: ReactNode
+}
+
+type ActivityContextProps = {
+    state: ActivityState
+    dispatch: ActionDispatch<[action: ActivityActions]>
+}
+
+export const ActivityContext = createContext<ActivityContextProps>(null!)
+
+export const ActivityProvider = ({children} : ActivityProviderProps) => {
+
+    const [state, dispatch] = useReducer(activityReducer, initialState)
+
+    return (
+        <ActivityContext.Provider value={{
+            state,
+            dispatch
+        }}>
+
+            {children}
+        </ActivityContext.Provider>
+    )
+}
